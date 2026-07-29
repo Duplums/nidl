842011222e770aafb5d45affb0114cf273520ef2
Benoit Dufumier <ben.dufumier@gmail.com>
2026-07-29 12:06:30 +0200

    multineurojepa proposition

--- nidl/estimators/ssl/__init__.py
+++ nidl/estimators/ssl/__init__.py
@@ -12,6 +12,7 @@ from .barlowtwins import BarlowTwins
 from .dcl import DCL
 from .dino import DINO
 from .ijepa import IJEPA
+from .multi_neurojepa import MultiNeuroJEPA
 from .neurojepa import NeuroJEPA
 from .simclr import SimCLR
 from .yaware import YAwareContrastiveLearning
